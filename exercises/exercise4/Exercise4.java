dcf7aa004fe3b39836b490aa140783b939c7d03c
江玟慧 <chiangweihui@MacBook-Air.local>
2025-03-13 23:39:32 -0700

    Moved exercise4 into exercises directory

---
+++ exercises/exercise4/Exercise4.java
@@ -0,0 +1,35 @@
+package exercise4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        String [] patterns = {
+                "\\d+", //number
+                "[a-zA-Z]+", //only letters and spaces
+                "\\bJava\\b", //Java
+                "^Hello.*", //starting with "Hello"
+                ".*\\d{2,}.*", //two or more number
+        };
+        //Test string
+        String [] testStrings = {
+                "20905",
+                "My name is Emily",
+                "I am learning Java",
+                "Hello, How are you?",
+                "My birthday is July 27",
+        };
+
+        for (int i = 0; i<patterns.length; i++) {
+            Pattern pattern = Pattern.compile(patterns[i]);
+            Matcher positiveMatcher = pattern.matcher(testStrings[i]);
+            Matcher negativeMatcher = pattern.matcher("XYZ");
+
+            System.out.println("Pattern: " + patterns[i]);
+            System.out.println("Positive Matches: " + testStrings[i] + "): " + positiveMatcher.find());
+            System.out.println("Negative Matches (XYZ): " + negativeMatcher.find());
+            System.out.println();
+        }
+    }
+}
